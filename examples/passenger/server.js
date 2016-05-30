f85d0a7826048dbc97bef5d2857041d5b57ca553
renhao <renhao.eric@gmail.com>
2016-05-30 13:37:34 +0800

    Create project example directory structure.

---
+++ examples/passenger/server.js
@@ -0,0 +1,19 @@
+/**
+ * Created by mafengwo on 16/5/30.
+ */
+
+var app = new (require('express'))();
+var port = 3000;
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.listen(port, function(error) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    }
+});
