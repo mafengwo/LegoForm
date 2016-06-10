a16071a20a4795b2c17ab17d589b04c074f40d4f
renhao <renhao.eric@gmail.com>
2016-06-10 15:03:35 +0800

    Add sinon.js as a spy and stub provider for unit test.

--- karma.conf.js
+++ karma.conf.js
@@ -1,24 +1,22 @@
 /**
  * Created by mafengwo on 16/5/31.
  */
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
 
         // base path used to resolve all patterns (e.g. files, exclude)
         basePath: '',
 
         // frameworks to use
-        frameworks: ['mocha', 'chai'],
+        frameworks: ['mocha', 'chai', 'sinon'],
 
         // // list of files / patterns to load in the browser
         files: [
-            // 'bower_components/angular/angular.js',
-            // 'bower_components/angular-mocks/angular-mocks.js',
             'node_modules/underscore/underscore.js',
             'node_modules/angular/angular.js',
             'node_modules/angular-mocks/angular-mocks.js',
-            'src/*/*.js',
-            'test/services/*.spec.js'
+            'src/**/*.js',
+            'test/**/*.spec.js'
         ],
 
         // list of files to exclude
