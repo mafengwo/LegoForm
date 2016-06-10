ca0bb13f577c9db77a9e56766890a00ec1e62110
renhao <renhao.eric@gmail.com>
2016-06-10 15:00:57 +0800

    Add form service.

---
+++ src/services/formService.js
@@ -0,0 +1,117 @@
+/*
+ * Form Service 
+ */
+angular.module('LegoForm')
+    .service('formService', ['modelService', function (modelService) {
+
+        var _TYPE_DEF = {
+            STRING: 0,
+            NUMBER: 1,
+            ARRAY: 2,
+            OBJECT: 3
+        };
+
+        var _form = [];
+        /**
+         *
+         * @type {Object}
+         * @private
+         */
+        var _formDef = [];
+
+        var _legoDef = [];
+
+        var _helperMapping = {};
+
+        var _isSaved = false;
+
+        this.initFormDef = function (formDef) {
+
+
+            _formDef.length = 0;
+            _legoDef.length = 0;
+
+            Array.prototype.push.apply(_formDef, formDef);
+
+            _.each(_formDef, function (form, k) {
+
+                _legoDef = _legoDef.concat(form['lego']);
+
+                _.each(form['lego'], function (lego, kk) {
+                    _helperMapping[lego.name] = [k, kk];
+                });
+            });
+
+            if (!_isSaved) {
+                _fillFormWithDefaultValue();
+            }
+        };
+
+        this.isSaved = function () {
+            return _isSaved;
+        };
+
+        this.setSaved = function (bool) {
+            _isSaved = bool;
+        };
+
+        this.getForm = function () {
+            return _form;
+        };
+
+        var _fillFormWithDefaultValue = function () {
+
+            _.each(_formDef, function (f, k) {
+
+                if (k >= _form.length) {
+                    _form.push({});
+                }
+
+                _.each(f.lego, function (lego) {
+
+                    var modelId = lego.name.split('$$')[2];
+
+                    if (modelService.getModelById(modelId).relationship === 1) {
+
+                        var formIndex = _helperMapping[lego.name][0];
+                        _form[formIndex][lego.name] = [];
+
+                    } else {
+
+                        if (!_.has(_form[k], lego.name)) {
+
+                            var schema = modelService.getSchemaByLegoName(lego.name), defaultValue;
+
+                            switch (schema['column_data_type']) {
+
+                                case _TYPE_DEF.STRING:
+                                    defaultValue = schema['column_default_value'] || '';
+                                    break;
+
+                                case _TYPE_DEF.NUMBER:
+                                    defaultValue = schema['column_default_value'] 
+                                        ? schema['column_default_value'] * 1 : 0;
+                                    break;
+
+                                case _TYPE_DEF.ARRAY:
+                                    defaultValue = schema['column_default_value'] 
+                                        ? JSON.parse(schema['column_default_value']) : [];
+                                    break;
+
+                                case _TYPE_DEF.OBJECT:
+                                    defaultValue = schema['column_default_value'] 
+                                        ? JSON.parse(schema['column_default_value']) : {};
+                                    break;
+
+                                default:
+                                    defaultValue = ''
+                            }
+
+                            _form[k][lego.name] = defaultValue;
+                        }
+                    }
+                });
+            });
+        }
+
+    }]);
---
+++ test/services/formService.spec.js
@@ -0,0 +1,73 @@
+describe("Unit test: FormService", function () {
+
+    var _Mock_FormDef = [
+        {
+            id: 1,
+            name: 'Basic Form',
+            lego: [
+                {
+                    name: 'Passenger$$name$$1$$1',
+                    required: 1,
+                    label: 'Name',
+                    type: 'Input',
+                    tips: ''
+                },
+                {
+                    name: 'Passenger$$gender$$1$$2',
+                    required: 1,
+                    label: 'Gender',
+                    type: 'Radio',
+                    options: [{label: 'Male', value: 0}, {label: 'Female', value: 1}],
+                    tips: 'Select Passenger\'s gender'
+                }
+            ]
+        }
+    ];
+
+
+    var formService, modelService;
+
+    beforeEach(module('LegoForm'));
+
+    beforeEach(inject(function ($injector) {
+
+        modelService = $injector.get('modelService');
+        formService = $injector.get('formService');
+    }));
+
+
+    it('should be initialized', function () {
+
+        var getSchemaByLegoNameFunc, form;
+        sinon.stub(modelService, 'getModelById').returns({id: 1, relationship: 0});
+        getSchemaByLegoNameFunc = sinon.stub(modelService, 'getSchemaByLegoName');
+        getSchemaByLegoNameFunc.withArgs('Passenger$$name$$1$$1').returns({
+            id: 1,
+            model_id: 1,
+            column: 'name',
+            column_data_default_value: '',
+            column_data_type: 0,
+            column_key_type: 1
+        });
+        getSchemaByLegoNameFunc.withArgs('Passenger$$gender$$1$$2').returns({
+            id: 2,
+            model_id: 1,
+            column: 'id',
+            column_data_default_value: '',
+            column_data_type: 1,
+            column_key_type: 0
+        });
+        formService.initFormDef(_Mock_FormDef);
+
+        form = formService.getForm();
+        
+        form.should.be.a('array');
+        expect(form).to.have.lengthOf(1);
+        expect(form[0]).to.have.property('Passenger$$name$$1$$1');
+        expect(form[0]).to.have.property('Passenger$$gender$$1$$2');
+        
+        assert.propertyVal(form[0], 'Passenger$$name$$1$$1', '');
+        assert.propertyVal(form[0], 'Passenger$$gender$$1$$2', 0);
+    })
+
+});
