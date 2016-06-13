5b3bf14965d72c9ca7ec9592520117dac4ee7be1
renhao <renhao.eric@gmail.com>
2016-06-14 00:21:22 +0800

    Add radio lego

---
+++ src/lego/radio/radio.js
@@ -0,0 +1,42 @@
+/**
+ * Created by mafengwo on 16/6/13.
+ */
+angular.module('LegoForm')
+    .controller('RadioController', ['$scope', function ($scope) {
+
+        var ngModelCtrl;
+
+        if (!$scope.radioOptions) {
+            $scope.radioOptions = {};
+        }
+
+        this.init = function (_mgModelCtrl) {
+
+            ngModelCtrl = _mgModelCtrl;
+        };
+
+        $scope.select = function (one) {
+
+            $scope.selected = one;
+            ngModelCtrl.$setViewValue(one.value);
+        }
+    }])
+    .directive('legoRadio', function () {
+        return {
+            require: ['legoRadio', 'ngModel'],
+            restrict: 'E',
+            scope: {
+                radioOptions: '=?'
+            },
+            templateUrl: 'radio.html',
+            controller: 'RadioController',
+            link: function (scope, elem, attrs, ctrls) {
+
+                var radioCtrl = ctrls[0],
+                    ngModelCtrl = ctrls[1];
+
+                radioCtrl.init(ngModelCtrl);
+
+            }
+        }
+    });
