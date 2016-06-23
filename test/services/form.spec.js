6196ce4cea1daa0e6b6b850f982feebd961a2586
renhao <renhao.eric@gmail.com>
2016-06-23 19:25:49 +0800

    feat:(form):add test case

--- test/services/form.spec.js
+++ test/services/form.spec.js
@@ -1,74 +1,39 @@
 describe("Unit test: FormService", function () {
 
-    var _Mock_FormDef = [
-        {
-            id: 1,
-            name: 'Basic Form',
-            lego: [
-                {
-                    name: 'Passenger$$name$$1$$1',
-                    required: 1,
-                    label: 'Name',
-                    type: 'Input',
-                    tips: ''
-                },
-                {
-                    name: 'Passenger$$gender$$1$$2',
-                    required: 1,
-                    label: 'Gender',
-                    type: 'Radio',
-                    options: [{label: 'Male', value: 0}, {label: 'Female', value: 1}],
-                    tips: 'Select Passenger\'s gender'
-                }
-            ]
-        }
-    ];
+    var formService, validationService,
+        formData = {},
+        legoDef = [
+            {name: 'lego_string', data_type: 'string'},
+            {name: 'lego_number', data_type: 'number'},
+            {name: 'lego_array', data_type: 'array'},
+            {name: 'lego_object', data_type: 'object'},
+            {name: 'lego_default_value', data_type:'object', default_value: {foo: 1}}
+        ];
 
-
-    var formService, modelService;
-
-    beforeEach(module('lf.service.model'));
+    beforeEach(module('lf.service.validation'));
     beforeEach(module('lf.service.form'));
 
     beforeEach(inject(function ($injector) {
 
-        modelService = $injector.get('modelService');
+        validationService = $injector.get('validationService');
         formService = $injector.get('formService');
+        formService.init(legoDef, formData);
     }));
 
 
-    it('should be initialized', function () {
-
-        var getSchemaByLegoNameFunc, form;
-        sinon.stub(modelService, 'getModelById').returns({id: 1, relationship: 0});
-        getSchemaByLegoNameFunc = sinon.stub(modelService, 'getSchemaByLegoName');
-        getSchemaByLegoNameFunc.withArgs('Passenger$$name$$1$$1').returns({
-            id: 1,
-            model_id: 1,
-            column: 'name',
-            column_data_default_value: '',
-            column_data_type: 0,
-            column_key_type: 1
-        });
-        getSchemaByLegoNameFunc.withArgs('Passenger$$gender$$1$$2').returns({
-            id: 2,
-            model_id: 1,
-            column: 'id',
-            column_data_default_value: '',
-            column_data_type: 1,
-            column_key_type: 0
-        });
-        formService.initFormDef(_Mock_FormDef);
-
-        form = formService.getForm();
+    it('should form data be initialized', function () {
         
-        form.should.be.a('array');
-        expect(form).to.have.lengthOf(1);
-        expect(form[0]).to.have.property('Passenger$$name$$1$$1');
-        expect(form[0]).to.have.property('Passenger$$gender$$1$$2');
-        
-        assert.propertyVal(form[0], 'Passenger$$name$$1$$1', '');
-        assert.propertyVal(form[0], 'Passenger$$gender$$1$$2', 0);
-    })
+        var generatedData = formService.getFormData();
+        generatedData.should.be.an('object');
+    });
+    
+    it('should be initialized with correct value', function() {
+        var generatedData = formService.getFormData();
+        generatedData['lego_string'].should.equal('');
+        generatedData['lego_number'].should.equal(0);
+        assert.deepEqual(generatedData['lego_array'], []);
+        assert.deepEqual(generatedData['lego_object'], {});
+        assert.deepEqual(generatedData['lego_default_value'], {foo: 1});
+    });
 
 });
